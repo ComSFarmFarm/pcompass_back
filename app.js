5d79bff7055639e0a086a2cc0e048cf18279d650
dhwoo <wooda01@naver.com>
2024-08-02 16:29:37 +0900

    news title scrapping 기능 추가

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import express from "express";
 import authRouter from "./controller/auth.js";
+<<<<<<< Updated upstream
+=======
+import articleRouter from "./controller/news_scraping.js";
+import db from "./postgresql.js";
+
+>>>>>>> Stashed changes
 
 const app  = express();
 
@@ -9,6 +15,7 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRouter);
+app.use("/news", articleRouter);
 
 app.listen(PORT, HOST, () => {
     console.log(`[LOG] Server is running on ${PORT}`);
