9abd36ab9ea9d18a0e6376900fa852b224a4f078
dhwoo <wooda01@naver.com>
2024-08-03 17:31:18 +0900

    get, response 수정

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express";
+import cors from 'cors';
+import requestIp from 'request-ip';
+
+import logger from './logger.js';
 import authRouter from "./controller/auth.js";
-<<<<<<< Updated upstream
-=======
 import articleRouter from "./controller/news_scraping.js";
 import db from "./postgresql.js";
 
->>>>>>> Stashed changes
 
 const app  = express();
 
@@ -14,8 +15,24 @@ const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(requestIp.mw());
+
 app.use("/auth", authRouter);
 app.use("/news", articleRouter);
+
+db.connect(err => {
+    if (err) {
+        console.log('database connection error', err.stack);
+    } else {
+        console.log('database connection success!');
+    }
+});
+
+app.get('/', (req, res) => {
+    logger.info({ip: req.clientIp, type: "/"});
+    return res.send('Hello, World!');
+})
 
 app.listen(PORT, HOST, () => {
     console.log(`[LOG] Server is running on ${PORT}`);
