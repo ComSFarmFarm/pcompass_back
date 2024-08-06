2002b9aafe8797e2873997c3b1743d022b7349b8
dhwoo <wooda01@naver.com>
2024-08-07 02:24:57 +0900

    feat(quiz): 첫 quiz 구현

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import requestIp from 'request-ip';
 
 import logger from './logger.js';
 import authRouter from "./controller/auth.js";
+import authQuiz from "./controller/quiz.js";
 import db from "./postgresql.js";
 
 
@@ -18,6 +19,7 @@ app.use(cors());
 app.use(requestIp.mw());
 
 app.use("/auth", authRouter);
+app.use("/quiz", authQuiz);
 
 db.connect(err => {
     if (err) {
